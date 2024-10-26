335fdf03981dbf9476ba91810f0a4266081bfa1e
Roland Breitschaft <3882369+danlor-dev@users.noreply.github.com>
2024-10-26 19:15:03 +0200

    chore: add some features

--- nodes/ConvertToItems/ConvertToItems.node.ts
+++ nodes/ConvertToItems/ConvertToItems.node.ts
@@ -34,13 +34,12 @@ export class ConvertToItems implements INodeType {
 				displayName: 'Search',
 				name: 'search',
 				type: 'string',
-				required: true,
 				default: '',
 				description: 'Value to search for in the defined search column',
 			},
 			{
 				displayName: 'Search by Column',
-				name: 'key',
+				name: 'searchBy',
 				type: 'string',
 				default: '',
 				description: 'Define the column name which should be used for searching',
@@ -53,48 +52,119 @@ export class ConvertToItems implements INodeType {
 				description: 'Comma-separated List of Fields to include in Result',
 				placeholder: 'e.g. field1,field2',
 			},
+			{
+				displayName: 'Options',
+				name: 'options',
+				type: 'collection',
+				placeholder: 'Add option',
+				default: {},
+				options: [
+					{
+						displayName: 'Include Rows with Empty Fields',
+						name: 'includeRowsWithEmptyFields',
+						type: 'boolean',
+						default: false,
+						description: 'Whether to include rows with empty fields in the result',
+					},
+				],
+			},
 		],
 	};
 
 	async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
-		const table = this.getNodeParameter('table', 0) as string;
+		const items = this.getInputData();
+
+		const tableInput = this.getNodeParameter('table', 0) as string;
 		const search = this.getNodeParameter('search', 0) as string;
-		const key = this.getNodeParameter('key', 0) as string;
+		const searchBy = this.getNodeParameter('searchBy', 0) as string;
 		const include = this.getNodeParameter('include', 0) as string;
+		const options = this.getNodeParameter('options', 0) as {
+			includeRowsWithEmptyFields: boolean;
+		};
 
-		const configs = ConvertToItems.convertTable(table, key, include);
-
-		let result: INodeExecutionData[] = [];
-		if (search && key) {
-			configs.forEach((config) => {
-				const value = config.json[key];
-				if (value === search) {
-					result.push(config);
-				}
-			});
-		} else {
-			result = configs;
+		let validFields: string[] = [];
+		if (include) {
+			validFields = include.split(',');
 		}
 
-		return [result];
+		if (validFields && validFields.length > 0) {
+			const table = ConvertToItems.convertTable(tableInput);
+
+			let result: INodeExecutionData[] = [];
+
+			items.forEach((item) => {
+				let itemResult: INodeExecutionData[] = [];
+				table.forEach((rows) => {
+					if (search && searchBy) {
+						const value = rows[searchBy];
+						if (value && value === search) {
+							itemResult.push({
+								json: {
+									...item.json,
+									...rows,
+								},
+							});
+						}
+					} else {
+						itemResult.push({
+							json: {
+								...item.json,
+								...rows,
+							},
+						});
+					}
+				});
+
+				// Add only Fields which are defined by validFields
+				const formattedResult: INodeExecutionData[] = [];
+				itemResult.forEach((x) => {
+					const tempItem: any = {};
+					validFields.forEach((field) => {
+						tempItem[field] = x.json[field];
+					});
+					formattedResult.push({
+						json: tempItem,
+					});
+				});
+				itemResult = formattedResult;
+
+				// Remove rows with empty fields
+				if (!options?.includeRowsWithEmptyFields) {
+					const cleanedResult: INodeExecutionData[] = [];
+					itemResult.forEach((row) => {
+						let isValid = true;
+						const keys = Object.keys(row.json);
+						keys.forEach((column) => {
+							const value = row.json[column];
+							if (!value) {
+								isValid = false;
+							}
+						});
+
+						if (isValid) {
+							cleanedResult.push(row);
+						}
+					});
+					itemResult = cleanedResult;
+				}
+
+				// Deploy the results
+				itemResult.forEach((x) => result.push(x));
+			});
+
+			return [result];
+		}
+
+		return [items];
 	}
 
-	private static convertTable(table: string, key: string, fields: string): INodeExecutionData[] {
+	private static convertTable(table: string): any[] {
 		const lines = table.split('\n');
 		const filteredLines = lines.filter(
 			(x) => !(x.startsWith('| -') || x.startsWith('|-')) && x.trim() !== '',
 		);
 
-		let validColumns: string[] = [];
-		if (fields) {
-			validColumns = fields.split(',');
-		}
-
-		if(key){
-			validColumns.push(key);
-		}
-
-		const result: INodeExecutionData[] = [];
+		const result: any[] = [];
 
 		const columns = filteredLines[0]
 			.split('|')
@@ -104,29 +174,19 @@ export class ConvertToItems implements INodeType {
 		filteredLines
 			.slice(1)
 			.map((row) => {
-				return row
-					.split('|')
-					.filter((x) => x.trim() !== '')
-					.map((x) => x.trim());
+				const rowResult = row.split('|').map((x) => x.trim());
+
+				return rowResult.slice(1, rowResult.length - 1);
 			})
 			.forEach((x) => {
 				const row: any = {};
 				for (let i = 0; i < x.length; i++) {
 					const column = columns[i];
-					if (validColumns.length === 0 || validColumns.includes(column)) {
-						const value = x[i];
-						if (value) {
-							row[column] = value;
-						} else {
-							row[column] = null;
-						}
-					}
+					row[column] = x[i];
 				}
 
 				if (row) {
-					result.push({
-						json: row,
-					});
+					result.push(row);
 				}
 			});
 
--- nodes/ConvertToItems/test/ConvertToItems.node.test.ts
+++ nodes/ConvertToItems/test/ConvertToItems.node.test.ts
@@ -1,6 +1,6 @@
 import { mock } from 'jest-mock-extended';
 
-import { IExecuteFunctions } from 'n8n-workflow';
+import { IExecuteFunctions, INodeExecutionData } from 'n8n-workflow';
 import { ConvertToItems } from '../ConvertToItems.node';
 
 describe('Topic', () => {
@@ -9,7 +9,7 @@ describe('Topic', () => {
 	beforeEach(() => {
 		jest.clearAllMocks();
 
-		executeFunctions.getNodeParameter.calledWith('table', 0).mockReturnValue(`
+		const table = `
 | entity_id       | room    | boot | shutdown |
 | --------------- | ------- | ---- | -------- |
 | switch.off_st_1 | office  | on   | off      |
@@ -17,27 +17,96 @@ describe('Topic', () => {
 | switch.kit_st_1 | kitchen | off  | off      |
 | switch.liv_st_1 | living  |      | off      |
 | switch.bat_st_1 | bath    | on   | on       |
-			`);
+`;
+
+		executeFunctions.getInputData.mockReturnValue([
+			{ json: { message: 'off', topic: 'smarthome/room/office/switch' } },
+		]);
+		executeFunctions.getNodeParameter.calledWith('table', 0).mockReturnValue(table);
+		executeFunctions.getNodeParameter.calledWith('options', 0).mockReturnValue({
+			includeRowsWithEmptyFields: false,
+		});
 	});
 
 	afterAll(() => {
 		jest.unmock('../ConvertToItems.node');
 	});
 
-	test('should parse a markdown table as object', async () => {
-		const items: any = await new ConvertToItems().execute.call(executeFunctions);
+	test('should only return input data', async () => {
+		const items: INodeExecutionData[][] = await new ConvertToItems().execute.call(executeFunctions);
+
 		expect(items).not.toBeNull();
-		expect(items[0][0].json.room).toEqual('office');
-		expect(items[0][4].json.room).toEqual('bath');
+		items[0].forEach(x => expect(x.json).not.toBeNull());
 	});
 
-	test('should parse a markdown table and filter the result', async () => {
+	test('should parse a markdown table and return only field rooms', async () => {
+		executeFunctions.getNodeParameter.calledWith('include', 0).mockReturnValue('room');
+
+		const items: INodeExecutionData[][] = await new ConvertToItems().execute.call(executeFunctions);
+		expect(items).not.toBeNull();
+		items[0].forEach(x =>{
+			const item = x.json as {
+				room: string
+			};
+
+			expect(item).not.toBeNull();
+			expect(item.room).not.toBeUndefined();
+			expect(['office', 'bath', 'kitchen', 'living']).toContain(item.room);
+		});
+	});
+
+	test('should parse a markdown table, filter for room office and return the result', async () => {
 		executeFunctions.getNodeParameter.calledWith('search', 0).mockReturnValue('office');
-		executeFunctions.getNodeParameter.calledWith('key', 0).mockReturnValue('room');
-		executeFunctions.getNodeParameter.calledWith('include', 0).mockReturnValue('boot');
+		executeFunctions.getNodeParameter.calledWith('searchBy', 0).mockReturnValue('room');
+		executeFunctions.getNodeParameter.calledWith('include', 0).mockReturnValue('entity_id,room,boot');
 
-		const items: any = await new ConvertToItems().execute.call(executeFunctions);
+		const items: INodeExecutionData[][] = await new ConvertToItems().execute.call(executeFunctions);
 		expect(items).not.toBeNull();
-		expect(items[0][0].json.room).toEqual('office');
+		items[0].forEach(x =>{
+			const item = x.json as {
+				entity_id: string;
+				room: string,
+				boot: string
+			};
+
+			expect(item).not.toBeNull();
+			expect(item.room).not.toBeUndefined();
+			expect(item.room).toEqual('office');
+			expect(item.entity_id.startsWith('switch.off_st_')).toBeTruthy();
+		});
 	});
+
+	test('should parse a markdown table, filter for room living and no result should returned', async () => {
+		executeFunctions.getNodeParameter.calledWith('search', 0).mockReturnValue('living');
+		executeFunctions.getNodeParameter.calledWith('searchBy', 0).mockReturnValue('room');
+		executeFunctions.getNodeParameter.calledWith('include', 0).mockReturnValue('room,boot');
+
+		const items: INodeExecutionData[][] = await new ConvertToItems().execute.call(executeFunctions);
+		expect(items).not.toBeNull();
+		expect(items[0][0]).not.toBeDefined();
+	});
+
+	test('should parse a markdown table, filter for room living and result should returned', async () => {
+		executeFunctions.getNodeParameter.calledWith('search', 0).mockReturnValue('living');
+		executeFunctions.getNodeParameter.calledWith('searchBy', 0).mockReturnValue('room');
+		executeFunctions.getNodeParameter
+			.calledWith('include', 0)
+			.mockReturnValue('room,shutdown');
+
+		const items: INodeExecutionData[][] = await new ConvertToItems().execute.call(executeFunctions);
+
+		expect(items).not.toBeNull();
+		items[0].forEach(x =>{
+			const item = x.json as {
+				room: string,
+				shutdown: string
+			};
+
+			expect(item).not.toBeNull();
+			expect(item.room).not.toBeUndefined();
+			expect(item.room).toEqual('living');
+			expect(item.shutdown).toEqual('off');
+		});
+	});
+
 });
